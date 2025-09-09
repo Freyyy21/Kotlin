7cbb18bdc607733fbe3495c586132afb75d4611f
Freyyy21 <dgraymaiden@gmail.com>
2025-09-09 15:48:35 +0700

    ControlFlow
    no2

--- src/main/kotlin/controlFlows/MatrixSum&When.kt
+++ src/main/kotlin/controlFlows/MatrixSum&When.kt
@@ -1,5 +1,32 @@
 package controlFlows
 
 fun main(){
+    val totalBarisMatrix = 3
+    val totalKolomMatrix = 3
+    val arrayMatrix = Array(3){ IntArray(3) }
 
+    //Mengisi Array
+    for (i in 0 until totalBarisMatrix){
+        for (j in 0 until totalKolomMatrix) {
+            print("Nilai matrix [${i+1}][${j+1}] = ")
+            arrayMatrix[i][j] = readln().toInt()
+        }
+    }
+
+    //Menjumlahkan isi array
+    var jumlahIsi = 0
+    for (i in 0 until totalBarisMatrix){
+        for (j in 0 until totalKolomMatrix) {
+            jumlahIsi += arrayMatrix[i][j]
+        }
+    }
+
+    println("Total = $jumlahIsi")
+    if (jumlahIsi < 10){
+        println("Kecil")
+    }else if (jumlahIsi in 10..20 ){
+        println("Sedang")
+    }else{
+        println("Besar")
+    }
 }
